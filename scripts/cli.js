d7ba269f46bd63b548383f8d3433ddfc685e5234
Salim KAYABASI <salim.kayabasi@gmail.com>
2022-05-17 11:38:59 +0200

    feat: reading package.json file if there is no arg (#49)

--- scripts/cli.js
+++ scripts/cli.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
 const later = require('..');
+const { readFile } = require('../src/fs');
 
-const ignoredArgs = ['-f', '--format', '--output-file', '-o'];
+const ignoredArgs = [
+  '-o',
+  '--output-file',
+  '-f',
+  '--format',
+  '--color',
+  '--no-color',
+];
 const args = process.argv.slice(2);
 
 let index = 0;
@@ -16,6 +24,25 @@ while (index < args.length) {
   } else {
     params.push(arg);
     index += 1;
+  }
+}
+
+if (!params.length) {
+  const file = `${process.cwd()}/package.json`;
+  const content = readFile(file);
+  if (content) {
+    try {
+      const { scripts = {} } = JSON.parse(content);
+      const lint = scripts.lint || scripts.eslint;
+      if (lint) {
+        const eslint = 'eslint';
+        const start = lint.indexOf(eslint) + eslint.length + 1;
+        const end = lint.indexOf('&&', start);
+        params.push(lint.slice(start, end).trim());
+      }
+    } catch (e) {
+      // ignore
+    }
   }
 }
 
